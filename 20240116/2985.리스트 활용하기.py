e32b5a87267a53470f8bf0f57db9107415070adf
grow.boribori <grow.boribori@gmail.com>
2024-01-17 12:22:59 +0900

    day2

---
+++ 20240116/2985.리스트 활용하기.py
@@ -0,0 +1,11 @@
+# 아래에 코드를 작성하시오.
+
+zero_list = [0]
+print(zero_list)
+many_zero_list = zero_list*250000
+print(len(many_zero_list))
+a = []
+for i in range(1, 11):
+    a.append(i)
+print(a)
+print(a[3:])
