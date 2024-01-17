35536d156e90a4fec0341b751d3e4c34dc891690
grow.boribori <grow.boribori@gmail.com>
2024-01-17 16:13:40 +0900

    day3

---
+++ 20240117/2215.덧셈 함수.py
@@ -0,0 +1,9 @@
+# 아래 함수를 수정하시오.
+def add_numbers(a, b):
+    sum = a + b
+    return sum
+a = 3
+b = 5
+sum = add_numbers(a, b)
+print(f'{a}과 {b}를 인자로 넘긴 경우, \n{sum}')
+# 수정한 add_numbers() 함수를 호출하시오.
