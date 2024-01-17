00d3e9b69f38ceef2b2aa8ecc48b2d468ba2f69a
grow.boribori <grow.boribori@gmail.com>
2024-01-17 12:09:12 +0900

    day1

---
+++ 20240115/1436.원의 넓이와 둘레 변수.py
@@ -0,0 +1,11 @@
+# 원주율
+pi = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679
+# 반지름
+radius = 15
+length = radius * 2* pi
+area = radius * radius * pi
+
+print(f'원주율 : {pi}')
+print(f'반지름 : {radius}')
+print(f'원의 둘레 : {length}')
+print(f'원의 넓이 : {area}')
