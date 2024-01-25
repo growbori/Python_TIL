6255b9e2b30d446b6d129131465001b32c43b66f
grow.boribori <grow.boribori@gmail.com>
2024-01-25 13:58:57 +0900

    day9

---
+++ 20240124/1454. Shape 클래스.py
@@ -0,0 +1,15 @@
+# main.py
+
+
+# 아래 클래스를 수정하시오.
+class Shape:
+    
+    def __init__(shape1, width, height):
+        shape1.width = width
+        shape1.height = height
+
+        
+
+
+shape1 = Shape(5, 3)
+print(shape1.width, shape1.height)
