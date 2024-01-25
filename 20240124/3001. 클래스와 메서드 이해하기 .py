a7edb2ddaeecedf8337056dde8c08f95a26f49aa
grow.boribori <grow.boribori@gmail.com>
2024-01-25 14:03:25 +0900

    3001

---
+++ 20240124/3001. 클래스와 메서드 이해하기 .py
@@ -0,0 +1,11 @@
+# 아래에 코드를 작성하시오.
+my_lsit = [1, 2, 3]
+my_dict = {'A': 1, 'B': 2, 'C': 3}
+
+# dir 함수를 사용하여 my_list와 my_dict가 가지고 있는 메서드 목록을 출력한다.
+print(dir(my_lsit))
+print(dir(my_dict))
+
+# help 함수를 사용하여 my_list와 my_dict가 가지고 있는 구성을 출력한다.
+print(help(my_lsit))
+print(help(my_dict))
