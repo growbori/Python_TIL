a725cd49fc7af9028e80b160fa29c11d81f8a236
grow.boribori <grow.boribori@gmail.com>
2024-01-29 17:37:02 +0900

    구간합

--- 20240129/구간합.py
+++ 20240129/구간합.py
@@ -9,7 +9,7 @@ for i in range(1, n+1):
     # N개의 정수, 구간 M개
     for j in range(a - b + 1):  # 첫번째 수부터 마지막 위치 a-b+1까지 범위에서
         newSum = 0 # b개씩 묶기 위한 초기합 0으로 세팅
-        for k in range(j, j + b):  # i부터 b번째 수까지(범위 b)로 해서 범위 내에서 구간 별로 묶음
+        for k in range(j, j + b):  # j부터 b번째 수까지(범위 b)로 해서 범위 내에서 구간 별로 묶음
             newSum += arr[k]  # newSum에 할당해서 순회하며 각 구간 합 구함
         sumList.append(newSum)  # newSum으로 구간 합 나타내고 구해진 값들을 sumList에 append함
 
