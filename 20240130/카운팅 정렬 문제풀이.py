2e1977f42b419cc542ea68194588f9a5d29140a1
grow.boribori <grow.boribori@gmail.com>
2024-01-30 09:50:49 +0900

    20240130

---
+++ 20240130/카운팅 정렬 문제풀이.py
@@ -0,0 +1,25 @@
+N = 6
+
+K = 9
+
+data = [7, 2, 4, 5, 2, 3] # 0~9, K = 9
+
+counts = [0] * (K + 1)
+ 
+temp = [0] * N # 누적합의 정렬된 결과를 넣을 빈 리스트
+# counts 배열에 기록하기
+
+for x in data:
+    counts[x] += 1
+
+# counts 누적합 구하기
+    
+for i in range(1, K + 1): # 1부터 K번 인덱스까지
+    counts[i] = counts[i-1] + counts[i] # 누적합 구하는 방법
+
+# data의 마지막 원소부터 정렬하기
+
+for i in range(N-1, -1, -1) : #N-1부터 0번째까지 감소하는 형태로 더해주기
+    counts[data[i]] -= 1 # 개수를 인덱스로 변환 (남은 개수 계산)
+    temp[counts[data[i]]] = data[i]
+print(*temp)
