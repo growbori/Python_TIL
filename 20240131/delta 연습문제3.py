9f68a9c91121d0d99693692cd3752f5da13c3e96
grow.boribori <grow.boribori@gmail.com>
2024-01-31 17:10:13 +0900

    색칠하기

--- 20240131/delta 연습문제3.py
+++
@@ -1,9 +0,0 @@
-di = [0, 1, 0, -1]
-dj = [1, 0, -1, 0]
-
-N = int(input())
-arr = [list(map(int, input().split() )) for _ in range(N)]
-total = 0
-
-for i in range(N):
-    for j in range(N)
---
+++ 20240131/색칠하기.py
@@ -0,0 +1,27 @@
+T = int(input()) # 테스트 케이스 수 입력 받기
+
+for t in range(T):
+    N = int(input())
+
+    arr = [list(map(int, input().split())) for n in range(N)]
+    new_arr = [[0] * 10 for _ in range(10)]
+    # print(arr)
+    for i in range(N):
+        c1, a1, c2, a2, color = arr[i][0], arr[i][1], arr[i][2], arr[i][3], arr[i][4]
+
+        for i in range(c1, c2+1):
+            for j in range(a1, a2+1):
+                new_arr[i][j] += color
+
+    result = 0
+    for i in range(10):
+        for j in range(10):
+            if new_arr[i][j] == 3:
+                result += 1
+
+    print(f'#{t+1} {result}')
+
+
+
+
+
