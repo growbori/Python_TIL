76a13f8f20c9f3ea4c3c935a28f9453912d6a38b
growbori <grow.boribori@gmail.com>
2024-02-06 23:56:56 +0900

    0206

--- 20240201/특별한 정렬 -2.py
+++ 20240201/특별한 정렬 -2.py
@@ -3,7 +3,7 @@ for test_case in range(1, T + 1):
     N = int(input())  # 정수 개수
     numbers = list(map(int, input().split())) # 리스트 형태로 숫자들을 받아오고
 
-    result = list() # 빈 리스트 생성
+    result = [] # 빈 리스트 생성
     while numbers: # numbers 리스트 안에 숫자가 있는 동안,
         max_number = numbers[0] # 가장 큰 숫자를 numbers[0] 이라고 가정
         min_number = numbers[0] # 가장 작은 숫자를 numbers[0] 이라고 가정
@@ -20,7 +20,7 @@ for test_case in range(1, T + 1):
         numbers.remove(min_number) # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 에서 이미 최솟값 1을 구해서 result에 작성했으므로 10 숫자를 제외한다.
 
 # 이렇게 계속 반복하다보면 맨 끝에 있는 최댓값, 최솟값이 result에 가서 적히게 된다!
-    print(f'#{test_case}', *result[:10] , end='\n')
+    print(f'#{test_case}', *result[:10] , end='\n') # 결괏값 언패킹해서 작성
     # print(*result[:10])
     
 # 테스트 케이스가 짝수여야 함! 홀수일 경우 case를 2개로 나눠서 계산해야 함!
