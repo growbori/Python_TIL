f225b999fec88f80f7fbc7ed6c81f7f0e43e7277
growbori <grow.boribori@gmail.com>
2024-02-23 11:26:46 +0900

    0223

---
+++ 20240223/비트 연산 실습 문제.py
@@ -0,0 +1,36 @@
+# print(0b11011110 & 0b11011)
+# print(0x4A3 | 25)
+#
+# KEY = 1004
+#
+# def encode_decode(num):
+#     return num ^ KEY
+#
+# print(encode_decode(1000))
+# print(encode_decode(4))
+#
+# print(bin(0b1101 << 2))
+# print(bin(0b1101 >> 2))
+#
+# t = 1
+# for i in range(5):
+#     print(bin(t), t)
+#     t = t << 1
+
+
+
+# M = 31
+# N = 5
+# def Test():
+#     tar = M
+#     for i in range(N):
+#         if tar & 0x1 == 0:
+#             return False
+#         tar = tar >> 1
+#     return  True
+#
+# print(Test())
+
+t = 0.1
+
+print(f'{t:.2f}')
