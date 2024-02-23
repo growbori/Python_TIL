071d2b1b0e0e519d1bcd0fef478d35b211d37ae9
growbori <grow.boribori@gmail.com>
2024-02-23 11:29:45 +0900

    0223

--- 20240221/회문.py
+++ 20240221/회문.py
@@ -1,23 +1,24 @@
-'''
-8 * 8 평면 글자판에 제시된 길이를 가진 회문의 갯수를 구하여라
-'''
-
-
-for tc in range(10):
-    N = int(input())
-    arr = [list(map(str, input().split())) for _ in range(8)]
-    count = 0
+T = int(input())
+ 
+for tc in range(1, T+1):
+    N, M = map(int, input().split())
+    arr = [input() for _ in range(N)]
+     
     # 가로 탐색
-    for i in range(8):
-        for j in range(8-N+1):
-            if arr[i][j:j+N] == arr[i][j:j+N][::-1]:
-                count += 1
+     
+    for i in range(N):
+        for j in range(N-M+1):
+            if arr[i][j: j + M] == arr[i][j: j + M][::-1]:
+                answer = arr[i][j : j+ M]
+ 
     # 세로 탐색
-    for i in range(8-N+1):
-        for j in range(8):
+ 
+    for i in range(N-M+1):
+        for j in range(N):
             vertical = ''
-            for x in range(i, i + T):
+            for x in range(i, i+M):
                 vertical += arr[x][j]
             if vertical == vertical[::-1]:
-                count += 1
-    print(count)
+                answer = vertical
+ 
+    print(f'#{tc} {answer}')
